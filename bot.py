72d71d37eca0add12dc7323515246a0efef46733
fsibot <kontakt@hska.info>
2011-09-21 01:33:25 +0800

    Import order is now correct in bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,13 @@
 # Add module and core folders to syspath
 # TODO: Is this the right way to do this?
 # see http://stackoverflow.com/questions/279237/python-import-a-module-from-a-folder
-import sys, os, config
+import sys, os
 sys.path.insert(0, "thirdparty/")
 sys.path.insert(0, "core/")
 sys.path.insert(0, "modules/")
 sys.path.insert(0, "modules/config/")
 
+import config
 from BotCore import FSIBot
 
 # Open config file
