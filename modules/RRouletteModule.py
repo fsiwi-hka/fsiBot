018c811bc2fceb81fa41cc9a3c276b1d17ff8519
psde <me@mathias-garbe.de>
2011-10-11 13:37:58 +0200

    RRoulette: Converted spaces to tabs, game now restarts after 2 hours

--- modules/RRouletteModule.py
+++ modules/RRouletteModule.py
@@ -3,39 +3,42 @@
 
 from BotModule import BotModule
 
-import os, sys, random
+import os, sys, random, time
 
 class RRouletteModule(BotModule):
-    def __init__(self):
-        self.size = 7
-        self.revolver = self.reload()
-        return
+	def __init__(self):
+		self.size = 7
+		self.lastShot = 0
+		self.revolver = self.reload()
+		return
 
-    def reload(self):
-        rev = []
-        for n in range(0, self.size):
-            rev.append('empty')
+	def reload(self):
+		rev = []
+		for n in range(0, self.size):
+			rev.append('empty')
 
-        rev[random.randint(0,self.size-1)] = 'bullet'
+		rev[random.randint(0,self.size-1)] = 'bullet'
 
-        return rev
+		return rev
 
+	def command(self, nick, cmd, args, type):
+		# Reload revolver if ppl havent played in a while
+		if len(self.revolver) != self.size and time.time() > self.lastShot + 60*60*2:
+			self.revolver = self.reload()
+			self.sendPublicMessage("Starte neues Spiel.")
 
+		# let em shoot
+		if cmd == '!roulette' and type == 'public':
+			if self.revolver.pop() == 'bullet':
+				line = 'Bang!! ' + nick + ' geht von uns wie ein echter Mann... Neues Glück: Trommel aus ' + str(self.size) + ' Männer-Bonbon-Fächern... Ein Bonbon ist drin und tödlich.'
+				self.revolver = self.reload()
+			else:
+				line = '*click* - ' + nick + ' ist ein Glückspilz. Nächster?'
+			self.sendPublicMessage(line)
+			self.lastShot = time.time()
 
-    def command(self, nick, cmd, args, type):
-        #let em shoot
-        if cmd == '!roulette' and type != 'private':
-            if self.revolver.pop() == 'bullet':
-                line = 'Bang!! ' + nick + ' geht von uns wie ein echter Mann... Neues Glück: Trommel aus ' + str(self.size) + ' Männer-Bonbon-Fächern... Ein Bonbon ist drin und tödlich.'
-                self.revolver = self.reload()
-            else:
-                line = '*click* - ' + nick + ' ist ein Glückspilz. Nächster?'
-            self.sendPublicMessage(line)
+		return
 
-        return
-
-    def help(self, nick):
-        self.sendPrivateMessage(nick, '!roulette - Russisch Roulette - zeig was in dir steckt!')
-        return
-
-# vim: set ts=8 sw=4 tw=0 :
+	def help(self, nick):
+		self.sendPrivateMessage(nick, '!roulette - Russisch Roulette - zeig was in dir steckt!')
+		return
