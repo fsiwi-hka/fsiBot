e3a89d7980650eb6e1dc58b1d6c76e4a41f85184
psde <me@mathias-garbe.de>
2012-02-02 22:31:24 +0800

    Bot now removes faulty modules on exception instead of crashing

--- core/BotCore.py
+++ core/BotCore.py
@@ -233,8 +233,8 @@ class FSIBot(SingleServerIRCBot):
 						self.addModule(addmod)
 						self.sendPrivateMessage(nick, "Module '" + addmod + "' added.")
 						self.log(nick + " added module '" + addmod + "'")
-					except:
-						self.sendPrivateMessage(nick, "Exception returned.")
+					except Exception as e:
+						self.sendPrivateMessage(nick, "Exception returned: " + str(e))
 					return
 
 				if args[0].lower() == "rm" or args[0].lower() == "remove":
@@ -293,12 +293,14 @@ class FSIBot(SingleServerIRCBot):
 			return
 		
 		# every string that starts with an '!' is considered to be an command
-		if cmd.startswith("!"):
-			for module in self.activeModules:
+		try:
+			if cmd.startswith("!"):
 				module.command(nick, cmd, args, type)
-		else:
-			for module in self.activeModules:
+			else:
 				module.onMessage(type, e.arguments()[0])
+		except Exception as e:
+			self.sendPublicMessage("Module '" + str(module) + "' crashed with '" + str(e) + "', removing.")
+			del self.activeModules[self.activeModules.index(module)]
 
 	# Dumps the number of chat users to a logfile
 	def dump_users(self, c, e):
