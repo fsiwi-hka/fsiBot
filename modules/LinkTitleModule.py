4a80073933ebcc3fffb725f3572f9985ebb05337
Daniel Hoff <github@danielhoff.de>
2012-03-02 23:34:28 +0100

    debug foo

--- modules/LinkTitleModule.py
+++ modules/LinkTitleModule.py
@@ -19,12 +19,11 @@ class LinkTitleModule(BotModule):
 			for frag in args:
 				o = urlparse(frag)
 				if o.scheme == 'http':
-					try:
+#					try:
 						html = lxml.html.parse(frag)
 						self.sendPublicMessage('[' + o.hostname + '] ' + re.sub('\s{2,}', ' ', html.find('.//title').text.replace('\n','')))
-					except:
-						self.sendPublicMessage(sys.exc_info()[0])
-						pass
+#					except:
+#						pass
 
 	def command(self, nick, cmd, args, type):
 		return
