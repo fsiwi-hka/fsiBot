0245fbfba8bf92b05ab5093218c75e7c9e18e1ad
Daniel Hoff <github@danielhoff.de>
2012-03-02 22:39:57 +0100

    New module showing titles for links

---
+++ modules/LinkTitleModule.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# coding=utf8
+
+# Geklaut bei qchn.
+
+from BotModule import BotModule
+from urlparse import urlparse
+import lxml.html
+
+
+class LinkTitleModule(BotModule):
+	def __init__(self):
+		return
+
+	def onMessage(self, type, args):
+		if type == 'public':
+			args = args.split(' ')
+			for frag in args:
+				o = urlparse(frag)
+				if o.scheme == 'http':
+					self.sendPublicMessage('Link gefunden')
+					html = lxml.html.parse(frag)
+					self.sendPublicMessage('[' + o.host + '] ' + html.find('.//title').text)
+
+	def help(self, nick):
+		return
