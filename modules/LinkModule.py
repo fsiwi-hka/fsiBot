13653941cab418323b8d30a4da8c3a07bf7e46f3
Tim Roes <mail@timroes.de>
2012-03-03 10:42:48 +0100

    Cleaned up code a bit

--- modules/LinkModule.py
+++ modules/LinkModule.py
@@ -12,26 +12,26 @@ class LinkModule(BotModule):
 	def command(self, nick, cmd, args, type):
 		if cmd == "!l" or cmd == "!link":
 
+			# Open wikipage containing links
 			links = open(os.path.abspath(os.path.dirname(sys.argv[0])) + "/links", "r")
-			if len(args) != 0:
-				reg = re.compile('\^\W*(' + args[0] + ')\W*\|\W*([^\|]*?)\|', re.IGNORECASE)
-			else:
-				reg = re.compile('\^\W*([^\|]*)\W*\|\W*([^\|]*?)\|', re.IGNORECASE)
 
 			if len(args) == 0:
+				reg = re.compile('\^\W*([^\|]*)\W*\|\W*([^\|]*?)\|', re.IGNORECASE)
 				self.sendPrivateMessage(nick, 'Folgende URLs sind bekannt und können per !link <name> gepostet werden:')
 				# Just output all known links
 				for line in links:
 					link = reg.match(line)
 					if link:
 						self.sendPrivateMessage(nick, link.group(1) + ': ' + link.group(2))
-				return
 
-			# Fetch entry in table
-			for line in links:
-				link = reg.match(line)
-				if link:
-					self.sendPublicMessage(link.group(2))
+			else:
+				reg = re.compile('\^\W*(' + args[0] + ')\W*\|\W*([^\|]*?)\|', re.IGNORECASE)
+				# Fetch entry in table
+				for line in links:
+					link = reg.match(line)
+					if link:
+						self.sendPublicMessage(link.group(2))
+
 
 	def help(self, nick):
 		self.sendPrivateMessage(nick, "!link/!l [link label] - Gibt eine bestimmte URL aus. (http://hska.info/links).")
