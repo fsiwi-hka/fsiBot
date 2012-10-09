90431c62b2bae6ea630c49675187c4b7212da8c6
Florian Limberger <flo@snakeoilproductions.net>
2012-10-09 11:10:01 +0200

    Add bullshit generator module
    For now, fetch bs from bullshit.aiju.de. It's a private server, so implementing
    a own generator is planned.

---
+++ modules/BullshitModule.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from BotModule import BotModule
+import urllib2
+
+class BullshitModule(BotModule):
+	def __init__(self):
+		pass
+
+	def command(self, nick, cmd, args, type):
+		if cmd == "!bs":
+			ht = urllib2.urlopen("http://bullshit.aiju.de")
+			for i in range(0, 6):
+				ht.readline()
+			self.sendPublicMessage(ht.readline()[:-7])
+
+	def help(self, nick):
+		self.sendPrivateMessage(nick, "!bs - Print a stream of bullshit from http://bullshit.aiju.de")
